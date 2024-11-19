1d1ebdcc51242218ebd8f46c9e5426d91db85bc1
hienjoy <wisdom16no@naver.com>
2024-11-19 15:00:06 +0900

    feat: 라우팅 추가

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-import logo from "./logo.svg";
+import React from "react";
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
+import Main from "./pages/Main";
+import Like from "./pages/Like";
+import Write from "./pages/Write";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <ConditionalHeader />
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/like" element={<Like />} />
+          <Route path="/write" element={<Write />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
+
+const ConditionalHeader = () => {
+  const location = useLocation();
+  return location.pathname !== "/login" ? <Header /> : null;
+};
 
 export default App;
