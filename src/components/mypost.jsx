e9b32a1ca29f21d905aa4a773b09475d36764a96
somin2352 <somin030502@gmail.com>
2024-11-26 15:36:29 +0900

    axios get 요청 수정

--- src/components/mypost.jsx
+++ src/components/mypost.jsx
@@ -1,11 +1,25 @@
 import Hashtag from "./hashtag";
 import "./../styles/mypost.css";
-function Mypost({ post }) {
+
+function formatDate(dateString) {
+  const date = new Date(dateString);
+  // 로컬 시간대에 적용되도록 변경
+  return date
+    .toLocaleDateString("ko-KR", {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+    })
+    .replace(/\./g, "-")
+    .replace(/\s/g, ""); // YYYY-MM-DD 형식으로 반환
+}
+
+function Mypost({ nickname, post }) {
   return (
     <div className="mypage">
       <div className="post-info">
-        <p className="user">{post.user} (나의 글)</p>
-        <p className="write-date">{post.writeDate}</p>
+        <p className="user">{nickname} (나의 글)</p>
+        <p className="write-date">{formatDate(post.updatedAt || post.createdAt)}</p>
       </div>
       <div className="post-wrapper">
         <div className="post-content">{post.content}</div>
